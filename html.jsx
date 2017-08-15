e7ee8a2b44b109f767eedbd6893ccb2ce66cb49c
mohamedtwice <mohamedtwice@gmail.com>
2017-08-14 23:53:40 -0500

    works

--- html.jsx
+++ html.jsx
@@ -46,8 +46,8 @@ export default class Html extends Component {
           <link rel="icon" type="image/png" href={require('assets/img/favicon/favicon-32x32.png')} sizes="32x32" />
           <link rel="icon" type="image/png" href={require('assets/img/favicon/favicon-16x16.png')} sizes="16x16" />
           <link rel="apple-touch-icon" sizes="180x180" href={require('assets/img/favicon/apple-touch-icon.png')} />
+
           <meta name="theme-color" content="#000000" />
-          <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
 
           {css}
         </head>
