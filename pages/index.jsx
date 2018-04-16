b3de325ab65c0731e8cb47423253dad1a943fb3d
Mohamed <mhm@umn.edu>
2018-04-16 14:42:21 -0500

    added cehd link

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ const Index = () => (
     <Fullscreen className={styles.intro} firstItem>
       <Wrapper>
         <h1>
-          <span class="styles.introhello">Hello. I’m Mohamed and I’m a full stack developer.</span>
+          <span class="styles.introhello">Hello. I’m Mohamed and I’m a full stack developer currently working for the <a className={styles.link} href="http://www.cehd.umn.edu">University of Minnesota</a>.</span>
           <br /><br />
           <b>Check out my <Link className={styles.link} to={prefixLink('/work/')}>work</Link>.</b>
         </h1>
